d6be418b07ce9256b75cc787d21f97d490dd8de6
MrPhilip7 <72625726+MrPhilip7@users.noreply.github.com>
2021-09-14 20:47:41 +0200

    Block Spawner

---
+++ RGBgame2/Assets/BlockSpawner.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour {
+    public Transform[] spawnPoints;
+
+    public GameObject blockPrefab;
+
+    public float wavesTime = 1f;
+
+    private float spawnTime = 2f;
+
+    // Start is called before the first frame update
+    private void Update() {
+        if (Time.time >= spawnTime) {
+            SpawnBlocks();
+            spawnTime = Time.time + wavesTime;
+        }
+    }
+
+    private void SpawnBlocks() {
+        int randomIndex = Random.Range(0, spawnPoints.Length);
+
+        for (int i = 0; i < spawnPoints.Length; i++) {
+            if (randomIndex != i) {
+                Instantiate(blockPrefab, spawnPoints[i].position, Quaternion.identity);
+            }
+        }
+    }
+}
