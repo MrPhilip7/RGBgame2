be7f4c5b2a13b9268449ffead2e2518b80a0959a
MrPhilip7 <72625726+MrPhilip7@users.noreply.github.com>
2021-09-21 01:02:27 +0200

    Color Switcher

--- RGBgame2/Assets/BlockSpawner.cs
+++ RGBgame2/Assets/BlockSpawner.cs
@@ -6,44 +6,92 @@ public class BlockSpawner : MonoBehaviour {
     public Transform[] spawnPoints;
 
     public GameObject blockPrefab;
+    public GameObject blockPrefab2;
+    public GameObject blockPrefab3;
 
     public SpriteRenderer xrenderer;
-
-    private Color[] colors = new Color[6];
+    public SpriteRenderer xrenderer2;
+    public SpriteRenderer xrenderer3;
 
     public float wavesTime = 1f;
 
     private float spawnTime = 2f;
 
-    // Start is called before the first frame update
     private void Start() {
-        colors[0] = Color.red;
-        colors[1] = Color.green;
-        colors[2] = Color.blue;
+        ColorSwitcher();
     }
 
-    private void Update() {
+    private void FixedUpdate() {
         if (Time.time >= spawnTime) {
             SpawnBlocks();
+            SpawnBlocks2();
+            SpawnBlocks3();
             spawnTime = Time.time + wavesTime;
             ColorSwitcher();
         }
     }
 
     private void SpawnBlocks() {
-        int randomIndex = 3;
+        for (int i = 0; i < 1; i++) {
+            if (1f != i) {
+                Instantiate(blockPrefab, spawnPoints[0].position, Quaternion.identity);
+            }
+        }
+    }
 
-        for (int i = 0; i < spawnPoints.Length; i++) {
-            if (randomIndex != i) {
-                Instantiate(blockPrefab, spawnPoints[i].position, Quaternion.identity);
+    private void SpawnBlocks2() {
+        for (int i = 0; i < 1; i++) {
+            if (1f != i) {
+                Instantiate(blockPrefab2, spawnPoints[1].position, Quaternion.identity);
+            }
+        }
+    }
+
+    private void SpawnBlocks3() {
+        for (int i = 0; i < 1; i++) {
+            if (1f != i) {
+                Instantiate(blockPrefab3, spawnPoints[2].position, Quaternion.identity);
             }
         }
     }
 
     private void ColorSwitcher() {
-        xrenderer.color = colors[Random.Range(0, 3)];
-    }
+        switch (Random.Range(0, 5)) {
+            case 0:
+                xrenderer.color = Color.red;
+                xrenderer2.color = Color.green;
+                xrenderer3.color = Color.blue;
+                break;
 
-    // private void ColorChecker() {
-    //  }
+            case 1:
+                xrenderer.color = Color.red;
+                xrenderer2.color = Color.blue;
+                xrenderer3.color = Color.green;
+                break;
+
+            case 2:
+                xrenderer.color = Color.green;
+                xrenderer2.color = Color.blue;
+                xrenderer3.color = Color.red;
+                break;
+
+            case 3:
+                xrenderer.color = Color.green;
+                xrenderer2.color = Color.red;
+                xrenderer3.color = Color.blue;
+                break;
+
+            case 4:
+                xrenderer.color = Color.blue;
+                xrenderer2.color = Color.red;
+                xrenderer3.color = Color.green;
+                break;
+
+            case 5:
+                xrenderer.color = Color.blue;
+                xrenderer2.color = Color.green;
+                xrenderer3.color = Color.red;
+                break;
+        }
+    }
 }
