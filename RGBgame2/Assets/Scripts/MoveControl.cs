f68e55c2cfc826b8c7432f849417f7e8eee2e568
MrPhilip7 <72625726+MrPhilip7@users.noreply.github.com>
2022-02-10 13:55:04 +0100

    Dodania możliwość poruszania strzałkami

--- RGBgame2/Assets/Scripts/MoveControl.cs
+++ RGBgame2/Assets/Scripts/MoveControl.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 
-public class MoveControl : MonoBehaviour
-{
+public class MoveControl : MonoBehaviour {
     private Rigidbody2D rb;
 
     public float moveSpeed;
@@ -9,45 +8,37 @@ public class MoveControl : MonoBehaviour
     private bool moveLeft, moveRight;
 
     // Start is called before the first frame update
-    private void Start()
-    {
+    private void Start() {
         rb = GetComponent<Rigidbody2D>();
         //moveSpeed = 5f;
         moveLeft = false;
         moveRight = false;
     }
 
-    public void MoveLeft()
-    {
+    public void MoveLeft() {
         moveLeft = true;
     }
 
-    public void MoveRight()
-    {
+    public void MoveRight() {
         moveRight = true;
     }
 
-    public void StopMoving()
-    {
+    public void StopMoving() {
         moveLeft = false;
         moveRight = false;
         rb.velocity = Vector2.zero;
     }
 
     // Update is called once per frame
-    private void Update()
-    {
-        if ((moveLeft || Input.GetKey(KeyCode.A)) && rb.transform.position.x > -2f)
-        {
+    private void Update() {
+        if ((moveLeft || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) && rb.transform.position.x > -2f) {
             rb.velocity = new Vector2(-moveSpeed, 0f);
         }
-        else
-        {
+        else {
             rb.velocity = Vector2.zero;
         }
 
-        if ((moveRight || Input.GetKey(KeyCode.D)) && rb.transform.position.x < 2f)
-        {
+        if ((moveRight || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) && rb.transform.position.x < 2f) {
             rb.velocity = new Vector2(moveSpeed, 0f);
         }
     }
